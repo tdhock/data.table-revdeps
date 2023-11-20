565f18035a6a38efa34469829e0b4bf275ee9062
Toby Dylan Hocking <toby.hocking@nau.edu>
2023-11-20 09:02:30 -0700

    remotes install stringi from github

--- params.R
+++ params.R
@@ -143,6 +143,7 @@ for(R.i in seq_along(R.vec)){
     R.e('install.packages("RODBC",configure.args="--with-odbc-manager=odbc")')
     R.e('install.packages("slam");install.packages("Rcplex",configure.args="--with-cplex-dir=/home/th798/cplex")')#conda install -c ibmdecisionoptimization cplex only installs python package, need to register on IBM web site, download/install cplex, then install.packages slam, then install packages Rcplex with configure args.
     R.e('dep <- read.csv("~/genomic-ml/data.table-revdeps/popular_deps.csv")$dep;ins <- rownames(installed.packages());print(some <- dep[!dep %in% ins]);install.packages(some)')
+    R.e('remotes::install_github("gagolews/stringi")')#https://github.com/tdhock/data.table-revdeps/issues/2#issuecomment-1817292386 installation bug in Nov 2023 CRAN version of stringi, fixed on github.
   }
   R.java.cmd <- paste(R, "CMD javareconf")
   system(R.java.cmd)
