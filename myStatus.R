06f1bcb04fcd23294e28ee4aabb3b1f4bab1ac7b
Toby Dylan Hocking <toby.hocking@nau.edu>
2024-02-22 22:56:30 -0700

    do not use --as-cran

--- myStatus.R
+++ myStatus.R
@@ -2,7 +2,7 @@ get_check_cmd <- function(pkg.tar.gz){
   paste(
     "_R_CHECK_FORCE_SUGGESTS_=0",
     R.home("bin/R"),
-    "CMD check --as-cran",
+    "CMD check --timings",
     pkg.tar.gz)
 }
 ## find and replace for significant differences.
